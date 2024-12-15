f14ec9a4865a5d16b0610053a8f6a9aaf83f5b8d
aspunith <punithsujatha@gmail.com>
2024-12-15 17:16:46 +0530

    Health Insurance Premium Prediction

---
+++ artifacts/sample.py
@@ -0,0 +1,2 @@
+import os
+print(os.path.exists("C:/Users/punit/OneDrive/Desktop/AI ML/Machine Learning/Project_1_Healthcare_Premium_Prediction(Regression)/Health Premium prediction app/artifacts/model_rest.joblib"))  # Should print True if the file exists
